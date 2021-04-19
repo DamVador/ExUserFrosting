0eafef83caaa97fe42ca48da53b29aa526359d87
Damien <damienbanville@hotmail.fr>
2021-04-19 23:20:56 +0200

    database finally setted up

---
+++ app/sprinkles/site/config/default.php
@@ -0,0 +1,27 @@
+<?php
+
+    /**
+     * Sample site configuration file for UserFrosting. You should definitely set these values!
+     *
+     */
+    return [
+        'address_book' => [
+            'admin' => [
+                'name'  => 'dam'
+            ]
+        ],
+        'debug' => [
+            'smtp' => true
+        ],
+        'site' => [
+            'author'    =>      'Dam',
+            'title'     =>      'Exercise',
+            // URLs
+            'uri' => [
+                'author' => 'https://attenboroughsreef.com'
+            ]
+        ],
+        'php' => [
+            'timezone' => 'Europe/London'
+        ]
+    ];
